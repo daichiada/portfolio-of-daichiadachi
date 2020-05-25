8acfacf38de62dafc59fc686883c39bc49b8215e
daichiadachi <bd1229igliteang48@gmail.com>
2020-05-25 17:04:16 +0900

    first commit

---
+++ js/main.js
@@ -0,0 +1,37 @@
+"use strict";
+// タイピング風表示　日本語ver.
+const typing = (element, sentence) => {
+  [...sentence].forEach((character, index) => {
+    setTimeout(() => {
+      document.querySelector(element).textContent += character;
+    }, 100 * ++index);
+  });
+};
+
+typing("#greeting-japanese", "こんにちは、ポートフォリオへようこそ。");
+
+// タイピング表示　English ver.
+const typingEnglish = (element, sentence) => {
+  [...sentence].forEach((character, index) => {
+    setTimeout(() => {
+      document.querySelector(element).textContent += character;
+    }, 130 * ++index);
+  });
+};
+
+typingEnglish("#greeting-english", "Hello,Welcome to my Portfolio");
+
+// jQuery
+// ふわっと表示させる
+$(function () {
+  $(window).scroll(function () {
+    $(".fadein").each(function () {
+      var position = $(this).offset().top;
+      var scroll = $(window).scrollTop();
+      var windowHeight = $(window).height();
+      if (scroll > position - windowHeight + 200) {
+        $(this).addClass("active");
+      }
+    });
+  });
+});
